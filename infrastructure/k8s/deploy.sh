f09228f0b78bdbd332c4810d2d3d8cde96040914
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-06-16 17:08:56 +0200

    test deploy script

--- infrastructure/k8s/deploy.sh
+++ infrastructure/k8s/deploy.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
+set -e
+PROJECT_ID=hapi-api-243910
+IMAGE=gcr.io/$PROJECT_ID/hapi-api:$CI_REPO_NAME.$CI_COMMIT_ID
+KUBERNETES_APP_NAME=hapi-api
+
 codeship_google authenticate
 
+gcloud container clusters get-credentials $KUBERNETES_APP_NAME
+
+echo $PROJECT_ID
+echo $IMAGE
+echo $KUBERNETES_APP_NAME
