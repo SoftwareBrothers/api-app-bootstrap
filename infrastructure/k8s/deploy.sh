e82a5ebe720ae212b3a06fb78c152b263acde99c
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-06-16 17:38:53 +0200

    fix

--- infrastructure/k8s/deploy.sh
+++ infrastructure/k8s/deploy.sh
@@ -2,11 +2,13 @@
 
 set -e
 PROJECT_ID=hapi-api-243910
-IMAGE=gcr.io/$PROJECT_ID/hapi-api:$CI_REPO_NAME.$CI_COMMIT_ID
 KUBERNETES_APP_NAME=hapi-api
+
+IMAGE=gcr.io/$PROJECT_ID/hapi-api:$CI_REPO_NAME.$CI_COMMIT_ID
 
 codeship_google authenticate
 
+gcloud config set compute/zone us-central1-a 
 gcloud container clusters get-credentials $KUBERNETES_APP_NAME
 
 echo $PROJECT_ID
