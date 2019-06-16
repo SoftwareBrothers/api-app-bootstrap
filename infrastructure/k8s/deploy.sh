5189eb3d8704d7d5d7edc64dd3d34ec6a3a4e9d9
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-06-16 17:47:07 +0200

    finish deployment

--- infrastructure/k8s/deploy.sh
+++ infrastructure/k8s/deploy.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+echo "Setting up env variables"
 set -e
 PROJECT_ID=hapi-api-243910
 KUBERNETES_APP_NAME=hapi-api
+STAGE=development
 
 IMAGE=gcr.io/$PROJECT_ID/hapi-api:$CI_REPO_NAME.$CI_COMMIT_ID
 
+echo "Setting up gcloud client"
 codeship_google authenticate
 
 gcloud config set compute/zone us-central1-a 
 gcloud container clusters get-credentials $KUBERNETES_APP_NAME
 
-echo $PROJECT_ID
-echo $IMAGE
-echo $KUBERNETES_APP_NAME
+sed -i "s,\$IMAGE_NAME,$IMAGE," ./deploy/infrastructure/k8s/$STAGE/api-deployment.yml
 
+echo "Deploying to Kubernetes..."
+kubectl apply -f ./deploy/infrastructure/k8s/$STAGE
 
-
-ls
+echo "Image $IMAGE was deployed"
